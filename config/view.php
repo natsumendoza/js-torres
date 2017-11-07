e8cb290877897565e07168e97affb77704c501a3
natsumendoza <mendozalaxus@yahoo.com>
2017-11-08 01:53:43 +0800

    fi

--- config/view.php
+++ config/view.php
@@ -28,6 +28,7 @@ return [
     |
     */
 
-    'compiled' => realpath(storage_path('framework/views')),
+//    'compiled' => realpath(storage_path('framework/views')),
+    'compiled' => storage_path().'/framework/views',
 
 ];
