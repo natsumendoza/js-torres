c3114072a46870f1fb6a32598d358e3b0bc10760
natsumendoza <mendozalaxus@yahoo.com>
2017-11-08 01:56:01 +0800

    fi

--- config/view.php
+++ config/view.php
@@ -28,7 +28,6 @@ return [
     |
     */
 
-//    'compiled' => realpath(storage_path('framework/views')),
-    'compiled' => storage_path().'/framework/views',
+    'compiled' => realpath(storage_path('framework/views')),
 
 ];
