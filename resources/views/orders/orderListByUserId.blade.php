1b022589209874d4bcee4b489cdb345d52764642
jhomaralglen <galanojhomar@gmail.com>
2017-11-09 00:55:13 +0800

    Image Directories

--- resources/views/orders/orderListByUserId.blade.php
+++ resources/views/orders/orderListByUserId.blade.php
@@ -55,25 +55,6 @@
 <script>
     $(document).ready(function() {
         alert("orderlistByUserId");
-        {{--var orders = <?php echo json_encode(@$orderList); ?>;--}}
-        {{--var orderImagePath = <?php echo json_encode(base_path('public/orderimages')); ?>;--}}
-
-        {{--$('.viewOrderImage').on('click',function (e) {--}}
-            {{--var id = e.target.id;--}}
-
-            {{--// FOR ANCHOR <a> TAG--}}
-            {{--$('#frontAnchorOrder').attr("href", orderImagePath + '/' + orders[id]['front_image']);--}}
-            {{--$('#backAnchorOrder').attr("href", orderImagePath + '/' + orders[id]['back_image']);--}}
-            {{--$('#leftAnchorOrder').attr("href", orderImagePath + '/' + orders[id]['left_image']);--}}
-            {{--$('#rightAnchorOrder').attr("href", orderImagePath + '/' + orders[id]['right_image']);--}}
-
-            {{--// FOR IMAGE src--}}
-            {{--$('#frontImgSrcOrder').attr("src", orderImagePath + '/' + orders[id]['front_image']);--}}
-            {{--$('#backImgSrcOrder').attr("src", orderImagePath + '/' + orders[id]['back_image']);--}}
-            {{--$('#leftImgSrcOrder').attr("src", orderImagePath + '/' + orders[id]['left_image']);--}}
-            {{--$('#rightImgSrcOrder').attr("src", orderImagePath + '/' + orders[id]['right_image']);--}}
-        {{--});--}}
-
     });
 </script>
 </html>
