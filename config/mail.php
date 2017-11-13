2f7612e649642f1740ad4267c8cfb120ad74f6d0
jhomaralglen <galanojhomar@gmail.com>
2017-11-13 16:51:01 +0800

    Edit mail

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'halepamilar@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'jstrs2010@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'JS Torres Admin'),
     ],
 
