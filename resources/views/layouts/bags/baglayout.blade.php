a3654276e02d491acade1cb4cf23e9a47532bc48
natsumendoza <mendozalaxus@yahoo.com>
2017-11-16 01:23:27 +0800

    fix

--- resources/views/layouts/bags/baglayout.blade.php
+++ resources/views/layouts/bags/baglayout.blade.php
@@ -82,7 +82,7 @@
             <a class="brand" href="{{ url('/') }}">June Sportshop</a>
             <div class="nav-collapse" id="main-menu">
                 <ul class="nav" id="main-menu-left">
-                    <li><a href="{{ url('/cutomize') }}">Jerseys</a></li>
+                    <li><a href="{{ url('/customize') }}">Jerseys</a></li>
                     <li><a href="{{ url('/bags') }}">Bags</a></li>
                 </ul>
             </div>
